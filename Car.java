26d0705d1730a46a69100b6c2c0d67130698b95c
Joshua Hamilton-Brown <joshuahb.jhb@gmail.com>
2024-09-26 19:02:27 +0300

    Include getters and setters for vehicle class, and move gitignore to main repo directory

--- Car.java
+++ Car.java
@@ -1,7 +1,8 @@
-public class Car {
+public class Car extends Vehicle{
     private final CarDriver currentDriver;
 
-    public Car(CarDriver currentDriver) {
+    public Car(String licensePlate, String make, String model, boolean isAvailable, boolean isRented, CarDriver currentDriver) {
+        super(licensePlate, make, model, isAvailable, isRented);
         this.currentDriver = currentDriver;
     }
 }
